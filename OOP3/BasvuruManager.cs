cf5bdd56368ede3780169ca029dd826c3ef488a9
Inky-Whale <berk.karanfiloglu@gmail.com>
2023-12-21 13:52:21 +0300

    m

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -3,12 +3,13 @@ namespace OOP3
 {
 	public class BasvuruManager
 	{
-		public void BasvuruYap(IKrediManager krediManager)
+		public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
 		{
 			//Başvuran bilgilerini değerlendirme
 			//
 
 			krediManager.Hesapla();
+			loggerService.Log();
 		}
 
 		public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -7,13 +7,17 @@ class Program
         IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
         IKrediManager tasitKrediManager = new TasitKrediManager();
         IKrediManager konutKrediManager = new KonutKrediManager();
+        ILoggerService fileLoggerService = new FileLoggerService();
 
         BasvuruManager basvuruManager = new BasvuruManager();
-        //basvuruManager.BasvuruYap(ihtiyacKrediManager);
-
+        basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService());
+        basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService);
+        basvuruManager.BasvuruYap(new EsnafKrediManager(), new SmsLoggerService());
         List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, konutKrediManager, tasitKrediManager };
 
-        basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
     }
 }
 
